ca09fa0847eb7182c9fdf1a7c339ec5b90f6c4c3
Jake <jacob.watson00@yahoo.com>
2024-11-01 01:13:58 -0600

    added more podcast links, and adjusted styling for links. Hover, color and decoration

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,7 +13,9 @@ function Hero() {
                 <p>Website coming Soon!</p>
                 <p>Links below to our most popular podcast platforms:</p>
                 <div className="links">
-                    <a href="https://feeds.buzzsprout.com/2221953.rss">Spotify</a>
+                    <a href="https://www.pandora.com/podcast/fire-and-white/PC:1001085627">Pandora</a>
+                    <a href="https://open.spotify.com/show/5CNtyoJ8vP9plKDmg3jVhv">Spotify</a>
+                    <a href="https://podcasts.apple.com/us/podcast/fire-white/id1733507614">Apple</a>
                 </div>
             </div>
         </div>
